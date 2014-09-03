2afb6c52dd0c9fdad8154c32ceb2dc9fb4befda2
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-09-03 09:59:59 +0200

    CS106B Section Handout Problem one complete

--- section-assignment-01/section-assignment-01.cpp
+++ section-assignment-01/section-assignment-01.cpp
@@ -7,14 +7,27 @@
 
 using namespace std;
 
-string CensorString(string text, string remove) {
+void CensorStringTwo(string & text, string remove) {
+
+    // Solution from Handouts. January 14, 2008
+    for(int i = 0; i < remove.length(); i++)
+    {
+        int pos = 0;
+        while ((pos = text.find(remove[i], pos)) != string::npos)
+        {
+            text.replace(pos, 1, ""); // replace char with empty string
+        }
+    }
+}
+
+string CensorStringOne(string text, string remove) {
     string return_string = "";
 
     bool found_character = false;
 
-    for (char& c : text)
+    for (char & c : text)
     {
-        for (char& r : remove)
+        for (char & r : remove)
         {
             if (c == r) {
                 found_character = true;
@@ -35,21 +48,26 @@ string CensorString(string text, string remove) {
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    string original_text;
 	string input_text;
     string output_text;
 	string remove_text;
 	string wait_to_exit;
 
 	cout << "Type your text:" << endl;
-	getline(cin, input_text);
+	getline(cin, original_text);
+    input_text = original_text;
 
     cout << "Type in your text to be removed" << endl;
     getline(cin, remove_text);
 
-    output_text = CensorString(input_text, remove_text);
+    output_text = CensorStringOne(input_text, remove_text);
+    CensorStringTwo(input_text, remove_text);
 
-    cout << "#####" << endl <<
-         "Output text: " << output_text << endl;
+    cout << "#####" << endl
+         << "Original text: " << original_text << endl
+         << "Input text: " << input_text << endl
+         << "Output text: " << output_text << endl;
 
     getline(cin, wait_to_exit);
 	return 0;
